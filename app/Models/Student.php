31d8d2c9a91b7ea50f8187c2bc71586d2b9b0810
Riyad <pycoder028@gmail.com>
2024-12-19 19:52:43 +0600

    Read data from db

---
+++ app/Models/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'registration_id',
+        'name',
+        'department_name',
+        'info'
+    ];
+
+}
